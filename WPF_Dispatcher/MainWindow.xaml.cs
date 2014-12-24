3033e82e21feb376188b50a9b29e59c0f1f5bf50
sujie <huaxia283611@hotmail.com>
2014-12-24 23:40:57 +0800

    WPF线程首次

---
+++ WPF_Dispatcher/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_Dispatcher
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.btn_test.Click += btn_test_Click;
+        }
+
+        void btn_test_Click(object sender, RoutedEventArgs e)
+        {
+            Thread th = new Thread(UpdateProgressBar);
+            th.Start();
+        }
+
+        private delegate void UpdateProgressBarDelegate(int i);
+
+        void UpdateProgressBar()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                //方法1
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    this.pb_test.Value = i;
+                }));
+                Thread.Sleep(100);
+
+                //方法2
+                //if (!this.pb_test.Dispatcher.CheckAccess())
+                //{
+                //    this.pb_test.Dispatcher.Invoke(
+                //        DispatcherPriority.Normal, 
+                //        new UpdateProgressBarDelegate((int progress) => { 
+                //            this.pb_test.Value = progress;
+                //        }),
+                //        i);
+                //    Thread.Sleep(100);
+                //    continue;
+                //}
+
+                //方法3
+                //if (!Dispatcher.CheckAccess())
+                //{
+                //    Dispatcher.Invoke(
+                //        DispatcherPriority.Normal, 
+                //        new UpdateProgressBarDelegate((int progress) => { 
+                //            this.pb_test.Value = progress; 
+                //        }), 
+                //        i);
+                //    Thread.Sleep(100);
+                //    continue;
+                //}
+            }
+        }
+
+    }
+}
