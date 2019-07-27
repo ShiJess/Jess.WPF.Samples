4d7679609b8bc9f279c0df75585397aaaabd1dad
Jess Smith <shisujie@outlook.com>
2019-07-27 15:19:26 +0800

    添加旧的wpfmvvm示例代码。

---
+++ MVVM/wpfmvvm/(Step1)WPF_MVVMBySelf/TestCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WPF_MVVMBySelf
+{
+    class TestCommand : ICommand
+    {
+      
+        public TestCommand(TestViewModel viemo)
+        {
+            viewmodel = viemo;
+        }
+
+        TestViewModel viewmodel{get;set;}
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        int i = 0;
+
+        /// <summary>
+        /// 命令是否可用
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 命令执行的操作
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            i++;
+            viewmodel.TestStr = i.ToString();
+        }
+
+    }
+}
---
+++ MVVM/wpfmvvm/(Step3)WPF_MVVM_PrismBasic/TestViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WPF_MVVM_PrismBasic
+{
+    class TestViewModel : NotificationObject
+    {
+        private string teststr;
+        /// <summary>
+        /// 待通知字符串
+        /// </summary>
+        public string TestStr
+        {
+            get { return teststr; }
+            set
+            {
+                teststr = value;
+                RaisePropertyChanged(()=>TestStr);
+            }
+        }
+
+        /// <summary>
+        /// 测试命令
+        /// </summary>
+        public ICommand TestCommand { get; set; }
+
+        /// <summary>
+        /// 测试带参数的命令
+        /// </summary>
+        public ICommand TestParameterCommand { get; set; }
+
+
+        public TestViewModel()
+        {
+            TestCommand = new DelegateCommand(Test,CanTest);
+
+            TestParameterCommand = new DelegateCommand<string>(TestParameter);
+        }
+
+        int i = 0;
+        /// <summary>
+        /// testcommand执行的方法
+        /// </summary>
+        private void Test()
+        {
+            i++;
+            TestStr = i.ToString();
+        }
+        /// <summary>
+        /// testcommand是否可用
+        /// </summary>
+        /// <returns></returns>
+        private bool CanTest()
+        {
+            return true;
+        }
+
+        private void TestParameter(string para)
+        {
+            TestStr = para + "para";
+        }
+    }
+}
